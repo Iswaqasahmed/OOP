9722fea23ff82437ec673afff08612eb51271a8a
waqas ahmed <37307194+waqasahmed123@users.noreply.github.com>
2020-11-26 17:27:23 +0500

    class file added

---
+++ class.py
@@ -0,0 +1,21 @@
+# simply clas:
+#     statement 1
+#      ...
+#      ...
+#      statement 2 
+
+
+class simple:
+    name = 'simple'
+    def information(self):
+        return 'information method'
+
+
+x = simple()
+print(x.name)
+print(x.information())
+
+
+
+
+    
