879cfd34b96902f519a4452356477233c9551a43
waqas ahmed <37307194+waqasahmed123@users.noreply.github.com>
2020-11-26 17:12:44 +0500

    gloabl and local scop added

--- namespace_variableScope.py
+++ namespace_variableScope.py
@@ -4,18 +4,39 @@
 # 3. Local Namespace.
 # 4. Enclosed Namespace.
 
-# print(dir())
+print(dir())
 
-# a = 5
-# print(id(a))
+myNameSpace = 10
+print(dir()) # its also show my myNameSpace 
 
-# b = 5
-# print(id(b))
 
-# a = 4
-# _ = 4
-# print(id(a))
-# print(id(b))
-# print(a+_)
+def scope_test():
+    def do_local():
+        spam = "local spam"
 
-print(__doc__)
+    def do_nonlocal():
+        nonlocal spam
+        spam = "nonlocal spam"
+
+    def do_global():
+        global spam
+        spam = "global spam"
+
+    spam = "test spam"
+    do_local()
+    print("After local assignment:", spam)
+    do_nonlocal()
+    print("After nonlocal assignment:", spam)
+    do_global()
+    print("After global assignment:", spam)
+
+scope_test()
+print("In global scope:", spam)
+
+a = 3
+def variable():
+    global a
+    a = 4
+    print(a)
+print(a)
+variable()
